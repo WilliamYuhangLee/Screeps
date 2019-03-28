bcdbd0fc6ff9d3dc129b496b15b2eb352868f07f
Yuhang Li <18326007+WilliamYuhangLee@users.noreply.github.com>
2019-03-28 08:57:57 -0400

    Refactor main loop and spawn logic

--- main.js
+++ main.js
@@ -1,10 +1,5 @@
 require("prototype.spawn")();
 
-var roleHarvester = require("role.harvester");
-var roleUpgrader = require("role.upgrader");
-var roleBuilder = require("role.builder");
-var roleRepairer = require("role.repairer");
-
 module.exports.loop = function () {
 
     for (let name in Memory.creeps) {
@@ -13,9 +8,9 @@ module.exports.loop = function () {
 
     let numberOfRolesWanted = {
         "harvester": 2,
-        "builder": 4,
-        "upgrader": 4,
-        "repairer": 1,
+        "builder": 6,
+        "upgrader": 6,
+        "repairer": 2,
     };
 
     for (let role in numberOfRolesWanted) {
@@ -28,14 +23,6 @@ module.exports.loop = function () {
 
     for (let name in Game.creeps) {
         let creep = Game.creeps[name];
-        if (creep.memory.role === "harvester") {
-            roleHarvester.run(creep);
-        } else if (creep.memory.role === "upgrader") {
-            roleUpgrader.run(creep);
-        } else if (creep.memory.role === "builder") {
-            roleBuilder.run(creep);
-        } else if (creep.memory.role === "repairer") {
-            roleRepairer.run(creep);
-        }
+        require("role." + creep.memory.role).run(creep);
     }
 };
--- prototype.spawn.js
+++ prototype.spawn.js
@@ -1,37 +1,16 @@
 util = require("util");
 module.exports = function () {
-    let harvester = [WORK, CARRY, MOVE];
-    let builder = [WORK, CARRY, MOVE];
-    let upgrader = [WORK, WORK, CARRY, MOVE, MOVE, MOVE];
-    let repairer = [WORK, CARRY, MOVE];
+    let bodies = {
+        "harvester": [WORK, WORK, WORK, WORK, CARRY, MOVE, MOVE],
+        "builder": [WORK, WORK, WORK, WORK, CARRY, MOVE, MOVE],
+        "upgrader": [WORK, WORK, WORK, WORK, CARRY, MOVE, MOVE],
+        "repairer": [WORK, WORK, WORK, CARRY, CARRY, CARRY, MOVE, MOVE],
+        "miner": [WORK, WORK, WORK, WORK, WORK, MOVE],
+    };
     StructureSpawn.prototype.spawnCustomCreep = function (energy, roleName) {
         let body = [];
-        switch (roleName) {
-            case "harvester":
-                for (let i = 0; i < Math.floor(energy / util.bodyCost(harvester)); i++) {
-                    body = body.concat(harvester);
-                }
-                break;
-            case "builder":
-                for (let i = 0; i < Math.floor(energy / util.bodyCost(builder)); i++) {
-                    body = body.concat(builder);
-                }
-                break;
-            case "upgrader":
-                for (let i = 0; i < Math.floor(energy / util.bodyCost(upgrader)); i++) {
-                    body = body.concat(upgrader);
-                }
-                break;
-            case "repairer":
-                for (let i = 0; i < Math.floor(energy / util.bodyCost(repairer)); i++) {
-                    body = body.concat(repairer);
-                }
-                break;
-            default:
-                for (let i = 0; i < Math.floor(energy / util.bodyCost(harvester)); i++) {
-                    body = body.concat(harvester);
-                }
-                break;
+        for (let i = 0; i < Math.floor(energy / util.bodyCost(bodies[roleName])); i++) {
+            body = body.concat(bodies[roleName]);
         }
         if (!body) return -6;
         let name = 1;
