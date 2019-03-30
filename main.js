4f036b27dd8c4e2eea2fe8db9cd2341c0476737b
Yuhang Li <18326007+WilliamYuhangLee@users.noreply.github.com>
2019-03-29 21:57:22 -0400

    Wrap main logic with profiler

--- main.js
+++ main.js
@@ -1,42 +1,59 @@
 require("prototype.spawn")();
+require("prototype.creep")();
+
+let modules = {
+    roles: {
+        harvester: require("role.harvester"),
+        builder: require("role.builder"),
+        upgrader: require("role.upgrader"),
+        repairer: require("role.repairer"),
+        miner: require("role.miner"),
+    }
+};
+
+const profiler = require("screeps-profiler");
+// Switch for turning on profiler
+profiler.enable();
 
 module.exports.loop = function () {
+    profiler.wrap(function () {
 
-    require("room")();
+        require("room")();
 
-    for (let name in Memory.creeps) {
-        if (!Game.creeps[name]) {
-            if (Memory.creeps[name].role === "miner") {
-                require("role.miner").clear(name);
-            } else {
-                delete Memory.creeps[name];
+        for (let name in Memory.creeps) {
+            if (!Game.creeps[name]) {
+                if (Memory.creeps[name].role === "miner") {
+                    modules.roles[Memory.creeps[name].role].clear(name);
+                } else {
+                    delete Memory.creeps[name];
+                }
             }
         }
-    }
 
-    let numberOfRolesWanted = {
-        "harvester": 3,
-        "builder": 6,
-        "upgrader": 6,
-        "repairer": 2,
-        "miner": Object.keys(Game.spawns["origin"].room.memory.containers).length,
-    };
+        let numberOfRolesWanted = {
+            "harvester": 2,
+            "builder": 2,
+            "upgrader": 4,
+            "repairer": 2,
+            "miner": Object.keys(Game.spawns["origin"].room.memory.containers).length,
+        };
 
-    for (let role in numberOfRolesWanted) {
-        let number = _.sum(Game.creeps, (creep) => creep.memory.role === role);
-        if (number < numberOfRolesWanted[role]) {
-            if (role !== "miner") {
-                let result = Game.spawns["origin"].spawnCustomCreep(Game.spawns["origin"].room.energyCapacityAvailable, role);
-                if (result === 0) break;
-            } else {
-                let result = require("role." + role).spawn(Game.spawns["origin"]);
-                if (result === 0) break;
+        for (let role in numberOfRolesWanted) {
+            let number = _.sum(Game.creeps, (creep) => creep.memory.role === role);
+            if (number < numberOfRolesWanted[role]) {
+                if (role !== "miner" && role !== "builder") {
+                    let result = Game.spawns["origin"].spawnCustomCreep(Game.spawns["origin"].room.energyCapacityAvailable, role);
+                    if (result === 0) break;
+                } else {
+                    let result = modules.roles[role].spawn(Game.spawns["origin"]);
+                    if (result === 0) break;
+                }
             }
         }
-    }
 
-    for (let name in Game.creeps) {
-        let creep = Game.creeps[name];
-        require("role." + creep.memory.role).run(creep);
-    }
+        for (let name in Game.creeps) {
+            let creep = Game.creeps[name];
+            modules.roles[creep.memory.role].run(creep);
+        }
+    })
 };
